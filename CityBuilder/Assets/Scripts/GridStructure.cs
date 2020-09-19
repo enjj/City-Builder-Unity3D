d0bc74a8c8ff5be5260af7bf2582631749817a28
Burak Ulusahan <burak.ulusahan@ogr.sakarya.edu.tr>
2020-09-19 22:06:33 +0300

    Add unit testing for grid structure

---
+++ CityBuilder/Assets/Scripts/GridStructure.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GridStructure  {
+
+    private int _cellSize = 3;
+
+    public GridStructure(int cellSize) {
+        _cellSize = cellSize;
+    }
+
+    public Vector3 CalculateGridPosition(Vector3 inputPosition) {
+        int x = Mathf.FloorToInt((float)inputPosition.x / _cellSize);
+        int z = Mathf.FloorToInt((float)inputPosition.z / _cellSize);
+        return new Vector3(x * _cellSize, 0, z * _cellSize);
+    }
+
+}
--- CityBuilder/Assets/Scripts/InputManager.cs
+++ CityBuilder/Assets/Scripts/InputManager.cs
@@ -7,14 +7,8 @@ using UnityEngine.UIElements;
 public class InputManager : MonoBehaviour {
 
     public LayerMask mouseInputMask;
-    public GameObject buildingPrefab;
-    public int cellSize = 3;
+    //public GameObject buildingPrefab;
 
-    void Start() {
-
-    }
-
-    // Update is called once per frame
     void Update() {
         GetInput();
     }
@@ -25,19 +19,13 @@ public class InputManager : MonoBehaviour {
             RaycastHit hit;
             if (Physics.Raycast(ray.origin,ray.direction,out hit,Mathf.Infinity,mouseInputMask)) {
                 Vector3 position = hit.point - transform.position;
-                Debug.Log(CalculateGridPosition(position));
-                CreateBuilding(CalculateGridPosition(position));
+                Debug.Log(position);
             }
         }
     }
 
-    public Vector3 CalculateGridPosition(Vector3 inputPosition) {
-        int x = Mathf.FloorToInt((float)inputPosition.x / cellSize);
-        int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
-        return new Vector3(x * cellSize, 0, z * cellSize);
-    }
-
-    public void CreateBuilding(Vector3 gridPos) {
-        Instantiate(buildingPrefab, gridPos, Quaternion.identity);
-    }
+   
+//    public void CreateBuilding(Vector3 gridPos) {
+//        Instantiate(buildingPrefab, gridPos, Quaternion.identity);
+//    }
 }
